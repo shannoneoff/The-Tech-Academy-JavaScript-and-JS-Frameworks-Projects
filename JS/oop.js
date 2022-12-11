5bf361fc3b4938a2f58c394290cbe83b0b2b3ffa
shannoneoff <shannon.eoff@gmail.com>
2022-12-11 09:09:09 -0800

    Add coder comments to JavaScript file

--- JS/oop.js
+++ JS/oop.js
@@ -1,12 +1,12 @@
-class Animal {
-    constructor(breed, color, height, weight) {
+class Animal {                                      //Create the class
+    constructor(breed, color, height, weight) {     //Construct the class with properties
         this.breed = breed;
         this.color = color;
         this.height = height;
         this.weight = weight;
     }
 
-    shake() {
+    shake() {                                       //Create methods for the class behaviors
         return "shake";
     }
 
@@ -19,8 +19,9 @@ class Animal {
     }
 }
 
-let myDog = new Animal("Hound", "brown", "2 feet", "60 pounds");
-document.getElementById("Dog").innerHTML =
-    "My dog is a " + myDog.color + " " + myDog.breed + ", which weighs " + 
+let myDog = new Animal("Hound", "brown", "2 feet", "60 pounds");    //Create an instance of an object
+                                                                    //using parent class properties
+document.getElementById("Dog").innerHTML =                          //Display the following string using
+    "My dog is a " + myDog.color + " " + myDog.breed + ", which weighs " +  //text, properties, and methods
     myDog.weight + " and is " + myDog.height + " tall.<br>It can " + 
     myDog.sit() + ", " + myDog.shake() + ", and " + myDog.layDown() + ".";
